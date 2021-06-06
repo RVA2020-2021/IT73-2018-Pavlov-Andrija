f0fc9eeb4a368bf2df928e760efc215582e15d54
unknown <pavlovandrija9@gmail.com>
2021-06-06 15:52:19 +0200

    Commit frontenda, Test 2

---
+++ Pavlov_Andrija_IT73_2018_FrontendRVA/src/app/app.constants.ts
@@ -0,0 +1,5 @@
+export const STATUS_URL = 'http://localhost:8083/status';
+export const FAKULTET_URL = 'http://localhost:8083/fakultet';
+export const DEPARTMAN_URL = 'http://localhost:8083/departman';
+export const STUDENT_URL = 'http://localhost:8083/student';
+export const STUDENTI_NA_DEPARTMANU_URL = 'http://localhost:8083/studentiNaDepartmanuId';
